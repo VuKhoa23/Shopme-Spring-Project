05a4dc21a69d167a06d27bfedbf59eb287b026c4
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-09-25 00:33:45 +0700

    Added: UserService with listAll() method, UserController with method to handle the /users endpoint

---
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -0,0 +1,16 @@
+package com.shopme.admin.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String listAll(){
+        return "users";
+    }
+}
