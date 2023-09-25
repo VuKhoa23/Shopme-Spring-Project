39e302919827c0aec775b6836cba1e8901e0571c
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-09-25 13:38:42 +0700

    Finished New User Feature: - Fill the form to add new user - JQueries to disables all other roles if admin roles is checked

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,10 +1,14 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,5 +23,22 @@ public class UserController {
         model.addAttribute("users", users);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        User user = new User();
+        user.setEnabled(true);
+        List<Role> allRoles = userService.listRoles();
+        model.addAttribute("user", user);
+        model.addAttribute("allRoles", allRoles);
+        return "user-form";
+    }
+
+    @PostMapping("/users/save")
+    public String handleFormSubmission(@ModelAttribute User user, RedirectAttributes redirectAttributes){
+        redirectAttributes.addFlashAttribute("message", "User added successfully");
+        userService.save(user);
+        return "redirect:/users";
     }
 }
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,18 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     public List<User> listAll(){
         return (List<User>)userRepository.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>)roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
